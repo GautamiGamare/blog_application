edb7b85090b81c475b471b42c6930db1fab8fdd1
Gautami <gautamigamare@gmail.com>
2024-07-28 22:25:04 +0530

    Add files via upload

---
+++ blog_application/README.md
@@ -0,0 +1,23 @@
+# Requirements
+ * Install the packages in requirements.txt
+   ```
+   [+] Open Terminal on the root directory and run the following
+        [-] pip install -r requirements.txt
+    ```
+ 
+# Procedure
+ * Fork the Repository [click here](https://github.com/akkupy/School-Management-System/fork)
+ * Clone the repository 
+ * Run python manage.py runserver.
+ * urls info
+    1. posts/ : list of all posts
+    2. posts/<pk> : retrieve specified post using primary key
+     ex. http://127.0.0.1:8000/posts/2
+    3. post_update/ : for Creating , Updating, Deleting post 
+     here just need to specify the method while calling the api.
+     for creating POST method, Updating PATCH and deleting DELETE method.
+     and For updating and deleting specify id/primary in body
+    4. comments/ : for getting list of all the comments
+    5. comments_update/ : for creating new comment.
+
+ 
---
+++ blog_application/blog_app/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from .models import *
+from .serializers import *
+
+
+# Create your views here.
+class posts_list(APIView):
+    def get(self, request, pk=None):
+        if pk:
+            data = Post.objects.get(id=pk)
+            serializer = PostSerializer(data)
+            return Response({'status':200, 'payload': serializer.data})
+        else:
+            data = Post.objects.all()
+            serializer = PostSerializer(data, many=True)
+            return Response(serializer.data)
+    
+class posts_update(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        data = request.data
+        serializer = PostSerializer(data = data)
+        if not serializer.is_valid():
+            return Response({'status':403,'errors':serializer.errors, 'message':'Something went wrong!'})
+        serializer.save()
+        return Response({'status':200, 'message':'Data saved!'})
+
+    def patch(self, request):
+        try:
+            data = Post.objects.get(id = request.data['id'])
+            serializer = PostSerializer(data, request.data, partial=True)
+            if not serializer.is_valid():
+                return Response({'status':403, 'error': serializer.errors, 'message': 'Something went wrong!'})
+            serializer.save()
+            return Response({'status':200, 'message':'Data saved!'})
+        except Exception as e:
+            return Response({'status': 400, 'message': 'Invalid id'})
+
+    def delete(self, request):
+        try:
+            id = request.GET.get('id')
+            data = Post.objects.get(id = id)
+            data.delete()
+            return Response({'status':200, 'message':'Data deleted!'})
+        except Exception as e:
+            return Response({'status': 400, 'message': 'Invalid id'})
+
+
+class comments_list(APIView):
+    def get(self, request):
+        comment_obj = Comment.objects.all()
+        serializer = CommentSerializer(comment_obj, many=True)
+        return Response(serializer.data)
+
+class comments_create(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        data = request.data
+        serializer = CommentSerializer(data = data)
+        if not serializer.is_valid():
+            return Response({'status':403,'errors':serializer.errors, 'message':'Something went wrong!'})
+        serializer.save()
+        return Response({'status':200, 'message':'Data saved!'})
